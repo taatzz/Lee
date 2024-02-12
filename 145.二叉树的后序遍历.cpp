85e915077a38cc4e128b66d3076bb42ed73bb634
taatzz <2679290837@qq.com>
2024-02-12 20:19:22 +0800

    2_12

--- 145.二叉树的后序遍历.cpp
+++ 145.二叉树的后序遍历.cpp
@@ -22,32 +22,52 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+// class Solution {
+// public:
+//     vector<int> res;
+
+//     void non_post(TreeNode* root)
+//     {
+//         stack<TreeNode*> s;
+//         s.push(root);
+
+//         while(!s.empty())
+//         {
+//             auto t = s.top();
+//             s.pop();
+//             res.push_back(t->val);
+//             if(t->left)
+//                 s.push(t->left);
+//             if(t->right)
+//                 s.push(t->right);
+//         }
+//         reverse(res.begin(), res.end());
+//     }
+
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         non_post(root);
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> res;
-
-    void non_post(TreeNode* root)
+    void dfs(TreeNode* root)
     {
-        stack<TreeNode*> s;
-        s.push(root);
+        if(!root) return;
 
-        while(!s.empty())
-        {
-            auto t = s.top();
-            s.pop();
-            res.push_back(t->val);
-            if(t->left)
-                s.push(t->left);
-            if(t->right)
-                s.push(t->right);
-        }
-        reverse(res.begin(), res.end());
+        dfs(root->left);
+        dfs(root->right);
+        res.push_back(root->val);
     }
 
     vector<int> postorderTraversal(TreeNode* root) {
-        non_post(root);
+        dfs(root);
+
         return res;
     }
 };
+
 // @lc code=end
 
